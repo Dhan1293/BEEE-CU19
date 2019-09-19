0096473ec18898f1133c9d0fdb42b9432ca8e2c8
Dhan1293 <54627513+Dhan1293@users.noreply.github.com>
2019-09-19 22:45:39 +0530

    Update LED_Flasher program.ino

--- Exp1_LED_Flasher/LED_Flasher program.ino
+++ Exp1_LED_Flasher/LED_Flasher program.ino
@@ -3,20 +3,20 @@
   Turns on an LED on for one second, then off for one second, repeatedly.
  */
  
-// Pin 13 has an LED connected on most Arduino boards.
+// Pin 9 has an LED connected on Arduino board.
 
 // the setup routine runs once when you press reset:
 
 
 void setup() {                
   // initialize the digital pin as an output.
-  pinMode(13 , OUTPUT);     
+  pinMode(9 , OUTPUT);     
 }
  
 // the loop routine runs over and over again forever:
 void loop() {
-  digitalWrite(13 , HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(9 , HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);               // wait for a second
-  digitalWrite(13 , LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(9 , LOW);    // turn the LED off by making the voltage LOW
   delay(1000);               // wait for a second
 }
