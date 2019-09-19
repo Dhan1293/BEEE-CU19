1f76a1c80c36aa6c4785e8580d20e1d86116701b
Dhan1293 <54627513+Dhan1293@users.noreply.github.com>
2019-09-19 22:43:42 +0530

    Update LED_Chaser program.ino

--- Exp2_LED_Chaser/LED_Chaser program.ino
+++ Exp2_LED_Chaser/LED_Chaser program.ino
@@ -1,20 +1,20 @@
 /*Program for LED chaser back and forth.
-  Pin 4, 5 and 6 are connected
+  Pin 2, 3, 4, 5, 6, 7, 8 and 9 are connected.
  */
  
 void setup() {                
-  for (int i=4; i<7; i=i+1){    // counting the variable i from 0 to 9
+  for (int i=2; i<=9; i=i+1){    // counting the variable i from 2 to 9
     pinMode(i, OUTPUT);            // initialising the pin at index i of the array of pins as OUTPUT
   }
 }
  
 void loop() {
-  for (int i=4; i<7; i=i+1){    // chasing right
+  for (int i=2 ; i<=9 ; i=i+1){    // chasing right
     digitalWrite(i, HIGH);         // switching the LED at index i on
     delay(100);                          // stopping the program for 100 milliseconds
     digitalWrite(i, LOW);          // switching the LED at index i off
   }
-  for (int i=6; i>4; i=i-1){   // chasing left (except the outer leds)
+  for (int i=9 ; i>=2; i=i-1){   // chasing left (except the outer leds)
     digitalWrite(i, HIGH);         // switching the LED at index i on
     delay(100);                          // stopping the program for 100 milliseconds
     digitalWrite(i, LOW);          // switching the LED at index i off
